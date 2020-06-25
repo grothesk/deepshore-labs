f8f6f6c00e2a155f273ed3754282daca54cad84e
Groth, Malte <malte.groth@deepshore.de>
2020-06-25 09:01:30 +0200

    Comments in build_push.sh

--- 03-von-der-applikation-zum-kubernetes-deployment/docker/build_push.sh
+++ 03-von-der-applikation-zum-kubernetes-deployment/docker/build_push.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+# Bedingt durch Dockerfile...
 cd ..
-# Build - Tags entsprechen <repo>:<version> im Register
+
+# Build - Tags entsprechen <repo>:<tag> im Register
 docker image build -f docker/Dockerfile -t grothesk/deeptalk-app:1.0.0 -t grothesk/deeptalk-app:latest .
-# Login
+
+# Login - Anmeldung beim Register
 docker login -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD"
-# Push
+
+# Push - Upload der Images
 docker image push grothesk/deeptalk-app:latest
 docker image push grothesk/deeptalk-app:1.0.0
