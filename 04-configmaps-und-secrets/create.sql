e07a061f74339713e73e4bc1e9c60856d68d7884
Groth, Malte <malte.groth@deepshore.de>
2020-09-15 16:00:25 +0200

    Added some config stuff

---
+++ 04-configmaps-und-secrets/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS listeners (
+	id serial PRIMARY KEY,
+	name VARCHAR ( 50 ) NOT NULL,
+	age INTEGER NOT NULL
+);
---
+++ 04-configmaps-und-secrets/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO listeners (name, age) VALUES
+    ('Malte', 29),
+    ('Frederic', 19),
+    ('Andy', 28),
+    ('Falk', 49),
+    ('Florian', 42)
+    ;
